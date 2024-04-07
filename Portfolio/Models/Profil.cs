083baf40ebe629dce9a4bc5ec2e0e5f656e339ae
AlexW1993 <alexander_william93@hotmail.com>
2024-04-07 16:28:05 -0400

    Add Forces Section

---
+++ Portfolio/Models/Profil.cs
@@ -0,0 +1,30 @@
+﻿namespace Portfolio.Models
+{
+    public class Profil
+    {
+        public List<string> Forces { get; set; }
+        public string Email { get; set; }
+
+        public Profil()
+        {
+            Forces = GetForces();
+            Email = "alexander_william93@hotmail.com";
+        }
+
+        private List<string> GetForces()
+        {
+            List<string> forces = new List<string>()
+            {
+                "Autonomy",
+                "Teamwork",
+                "Patience",
+                "Versatility",
+                "Tenancity",
+                "Rigor",
+                "Adapatation",
+            };
+
+            return forces;
+        }
+    }
+}
